3ca983ba63e8c64c94f50fca28b923baca06bc46
codeChef8888 <eryashre8888@gmail.com>
2022-03-23 17:19:33 +0545

    Book Feature Demo Under Debugging MultipartFile Upload

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+//Initializing the Environment Variables
 if(process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
+//Importing Express Package
 const express = require("express");
 const app =  express();
-//To access the Body of Request
+
+//Inorder To access the Body of Request
 app.use(express.urlencoded({ extended: true }));
+
+//Importing express-ejs-layouts package
 const expressLayouts = require("express-ejs-layouts");
 
+//Importing Different Routes
 const indexRouter =  require("./routes/index");
 const authorRouter =  require("./routes/authors");
+const bookRouter =  require("./routes/books");
 
 //Setting up the View
 app.set("view engine", "ejs");
@@ -26,8 +33,11 @@ const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to MongoDB"));
 
+//Hooking up our Different Routers to Route
 app.use("/",indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
+//Specifying the Port To Listen At with Default port: 3000
 app.listen(process.env.PORT || 3000);
 
